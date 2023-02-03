03eacd6d60d0a81d7fcc972142a0183a8d77b364
Jaidev1807 <jaidevpatel1807@gmail.com>
2023-02-02 20:45:40 -0500

    add

--- accordian.js
+++
@@ -1,8 +0,0 @@
-function openAccordian(section) {
-  var x = document.getElementById(section);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
